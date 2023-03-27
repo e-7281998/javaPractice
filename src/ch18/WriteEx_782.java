10aa6121fa45afba72bf0df13182b22fc8e8009c
e-7281998 <jejjcs43@naver.com>
2023-03-28 00:16:42 +0900

    18.바이트 출력 스트림

---
+++ src/ch18/WriteEx_782.java
@@ -0,0 +1,29 @@
+package ch18;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class WriteEx_782 {
+	public static void main(String[] args) {
+		try {
+			OutputStream os = new FileOutputStream("C:/temp/test1.db");
+			
+			byte a = 10;
+			byte b = 20;
+			byte c = 30;
+			
+			os.write(a);
+			os.write(b);
+			os.write(c);
+			
+			//내부 버퍼에 잔류하는 바이트 출력,버퍼 비우기
+			os.flush();
+			os.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
