38b1d3c9ecc39c8fa9db6326cc4b9441cbb40992
e-7281998 <jejjcs43@naver.com>
2023-04-10 09:06:19 +0900

    18.확인문제 10번 (폴더 생성 후 파일복사, 바이트 스트림 + 보조 스트림)

--- src/ch18/Check10.java
+++ src/ch18/Check10.java
@@ -2,38 +2,47 @@ package ch18;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.regex.Pattern;
+import java.nio.charset.Charset;
 
 public class Check10 {
-	public static void main(String[] args) throws IOException {
-		String originPath = "text1.txt";
-		String copyPath = "copyFile/text2.txt";
-		
-		InputStream fis = new FileInputStream(originPath);
-		BufferedInputStream bis = new BufferedInputStream(fis);
-		OutputStream fos = new FileOutputStream(copyPath);
-		BufferedOutputStream bos = new BufferedOutputStream(fos);
-		
- 		File file = new File("copyFile");
-		if(!file.exists())
-			file.mkdirs();
-		
-		while(true) {
-			
-			int str = bis.read();
-			if(str == -1)	break;
-			System.out.println(str);
+	public static void main(String[] args) throws IOException {	
+				
+ 		File ofile = new File("text1.txt");
+ 		File cfile = new File("copyFile");
+		String cPath = "copyFile/text2.txt";
+ 
+		System.out.println("원본 파일 경로 : " +ofile);
+ 		System.out.println("복사 파일 경로 : " +cPath);
+ 		
+		if(!ofile.exists()) {
+			System.out.println("원본 파일이 존재하지 않습니다.");
+			return;
 		}
+		if(!cfile.exists())
+			cfile.mkdir();
+		
+		FileInputStream is = new FileInputStream(ofile);
+		FileOutputStream os = new FileOutputStream(cPath);
+		BufferedInputStream bis = new BufferedInputStream(is);
+		BufferedOutputStream bos = new BufferedOutputStream(os);
+
+		while(true) {
+			int str = bis.read();
+			if(str == -1)
+				break; 
+			bos.write((char)str);
+		}
+		System.out.println(ofile + "파일을 "+cPath+"파일에 복사하였습니다.");
 		
 		bis.close();
-		fis.close();
+		is.close();
+		bos.flush();
 		bos.close();
-		fos.close();
+		os.close(); 
 	}
 }
